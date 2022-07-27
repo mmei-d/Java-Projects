8e7172333c154e86fca2c7f60254331de9aac922
mmei-d <meideng06@gmail.com>
2022-07-27 16:04:15 -0400

    Time: 150 ms (75.68%), Space: 69.2 MB (12.43%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -5,7 +5,9 @@ class Solution {
         }
         
         boolean[] numbers = new boolean[n];
+        // consider all possible multiples of all prime numbers below n
         for (int p = 2; p <= (int)Math.sqrt(n); p++) {
+            // if the number isn't marked as a multiple yet
             if (numbers[p] == false) {
                 for (int j = p*p; j < n; j += p) {
                     numbers[j] = true;
@@ -13,6 +15,7 @@ class Solution {
             }
         }
         
+        // count all numbers not marked as multiples (i.e. prime numbers)
         int numberOfPrimes = 0;
         for (int i = 2; i < n; i++) {
             if (numbers[i] == false) {
